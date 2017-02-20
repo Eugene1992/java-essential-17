2bf3c86fe1f0c07a320f907d0a8782908874eaa9
Eugene1992 <deyneko55@gmail.com>
2017-02-20 17:53:48 +0200

    Test hw done.

--- src/main/java/com/cbs/edu/Main.java
+++ src/main/java/com/cbs/edu/Main.java
@@ -6,7 +6,5 @@ package com.cbs.edu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 }
