39b6f0ff800c350ac5feab1ade97cf3433a40f2c
VladimirShuliak <123kpi123>
2017-02-21 14:59:27 +0200

    Development branch.

--- vladimir/src/main/java/com/cbs/edu/Main.java
+++ vladimir/src/main/java/com/cbs/edu/Main.java
@@ -1,10 +1,13 @@
 package com.cbs.edu;
 
 /**
- * Some comment.
+ * First comment.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
     }
 }
