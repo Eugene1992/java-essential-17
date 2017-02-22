9c2bcb53c0d255a93f0973cc0d6ae8a09e4b1bbd
VladimirShuliak <123kpi123>
2017-02-22 15:20:10 +0200

    Home work  01.

--- vladimir/src/main/java/com/cbs/edu/Main.java
+++ vladimir/src/main/java/com/cbs/edu/Main.java
@@ -1,11 +1,123 @@
 package com.cbs.edu;
 
 /**
- * Comment.
+ * Solution homework.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        Company vest = new Company();
+        vest.companyName = " Amster";
+        vest.city = " Amsterdam";
+        vest.capital = 900_000_000;
+        vest.amountOfWorkers = 789;
+        Customer vestCustomer = new Customer();
+        vestCustomer.company = " Fanta";
+        vestCustomer.name = " Alex";
+        vestCustomer.age = 32;
+        Project vestProject = new Project();
+        vestProject.startDate = 20;
+        vestProject.startMouns = 1;
+        vestProject.startYear = 2017;
+        vestProject.endDate = 20;
+        vestProject.endMouns = 2;
+        vestProject.endYear = 2017;
+        Sprint novusSprint = new Sprint();
+        novusSprint.firstSprint = " formation";
+        novusSprint.secondSprint = " completion";
+        novusSprint.thirdSprint = " delivery of the customer";
+        Task novusTask = new Task();
+        novusTask.hours = 132;
+
+        Company asel = new Company();
+        asel.companyName = " Asel";
+        asel.city = " Boston";
+        asel.capital = 300_000_0;
+        asel.amountOfWorkers = 342;
+        Customer aselCustumer = new Customer();
+        aselCustumer.company = " BoSota";
+        aselCustumer.name = " Neal";
+        aselCustumer.age = 64;
+        Project aselProject = new Project();
+        aselProject.startDate = 3;
+        aselProject.startMouns = 2;
+        aselProject.startYear = 2017;
+        aselProject.endDate = 6;
+        aselProject.endMouns = 2;
+        aselProject.startYear = 2017;
+        Sprint aselSprint = new Sprint();
+        aselSprint.firstSprint = " formation";
+        aselSprint.secondSprint = " completion";
+        aselSprint.thirdSprint = " delivery of the customer";
+        Task aselTask = new Task();
+        aselTask.hours = 27;
+
+        Company star = new Company();
+        star.companyName = " Star";
+        star.city = " London";
+        star.capital = 768_000_0;
+        star.amountOfWorkers = 39;
+        Customer starCustomer = new Customer();
+        starCustomer.company = " Sun";
+        starCustomer.name = " Kate";
+        starCustomer.age = 27;
+        Project starProject = new Project();
+        starProject.endDate = 14;
+        starProject.startMouns = 4;
+        starProject.startYear = 2017;
+        starProject.endDate = 28;
+        starProject.endMouns = 4;
+        starProject.endYear = 2017;
+        Sprint starSprint = new Sprint();
+        starSprint.firstSprint = " formation";
+        starSprint.secondSprint = " completion";
+        starSprint.firstSprint = " delivery of the customer";
+        Task starTask = new Task();
+        starTask.hours = 54;
+
+        Company perspective = new Company();
+        perspective.companyName = " Perspective";
+        perspective.city = " Zagreb";
+        perspective.capital = 239_000_00;
+        perspective.amountOfWorkers = 153;
+        Customer perspectiveCustomer = new Customer();
+        perspectiveCustomer.company = " Moon";
+        perspectiveCustomer.name = " James";
+        perspectiveCustomer.age = 45;
+        Project perspectiveProject = new Project();
+        perspectiveProject.startDate = 25;
+        perspectiveProject.startMouns = 5;
+        perspectiveProject.startYear = 2017;
+        perspectiveProject.endDate = 29;
+        perspectiveProject.endMouns = 5;
+        perspectiveProject.endYear = 2017;
+        Sprint perspectiveSprint = new Sprint();
+        perspectiveSprint.firstSprint = " perspective";
+        perspectiveSprint.secondSprint = " completion";
+        perspectiveSprint.thirdSprint = "  delivery of the customer";
+        Task perspectiveTask = new Task();
+        perspectiveTask.hours = 25;
+
+        Company logistic = new Company();
+        logistic.companyName = " Logistic";
+        logistic.city = " Paris";
+        logistic.capital = 780_000_0;
+        logistic.amountOfWorkers = 67;
+        Customer logisticCustomer = new Customer();
+        logisticCustomer.company = " Suits";
+        logisticCustomer.name = " Megan";
+        logisticCustomer.age = 53;
+        Project logisticProject = new Project();
+        logisticProject.startDate = 30;
+        logisticProject.startMouns = 9;
+        logisticProject.startYear = 2018;
+        logisticProject.endDate = 18;
+        logisticProject.endMouns = 05;
+        logisticProject.endYear = 2019;
+        Sprint logisticSprint = new Sprint();
+        logisticSprint.thirdSprint = " perspective";
+        logisticSprint.secondSprint = " completion";
+        logisticSprint.firstSprint = " delivery of the customer";
+        Task logisticTask = new Task();
+        logisticTask.hours = 320;
     }
 }
---
+++ vladimir/src/main/java/com/cbs/edu/Project.java
@@ -0,0 +1,14 @@
+package com.cbs.edu;
+
+/**
+ * Duration of the project.
+ * Created by Vladimir on 21.02.2017.
+ */
+public class Project {
+    int startDate;
+    int startMouns;
+    int startYear;
+    int endDate;
+    int endMouns;
+    int endYear;
+}
